f681dd45110a675d6763a23d21f9cd936b0ef22e
egisantika27 <egi.santika27@gmail.com>
2025-09-03 15:06:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const linksData = [
         {
-            title: "Cara Penggunaan",
+            title: "Fitur & Cara Penggunaan",
             url: "https://shoper.myscalev.com/shoper-shopee-product-research-tool"
         },
         {
@@ -96,4 +96,5 @@ document.addEventListener('DOMContentLoaded', function() {
     renderSocials();
 
 });
+
 
